3d7d8830d05e0cac54e16aa62f509f3363c1082a
mosss.os <mosescsunday1@gmail.com>
2026-05-05 16:14:09 +0100

    fix: Fix metro.config.js syntax error

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Ensure .ts and .tsx files can be resolved
-config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', ...config.resolver.sourceExts];
+config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', ...(config.resolver.sourceExts || [])];
 
 module.exports = config;
